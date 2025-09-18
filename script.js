1b95fcae6aa61f7faac48b3d027ccc8c391106a7
AlexandreSoares <soaresallexandre28@gmail.com>
2025-09-18 10:01:08 -0300

    Alterado os numeros de telefone para números aleátorios

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ const searchButton = document.querySelector(".search-button");
 const contactName = document.querySelector(".input-search");
 
 const contacts = [
-    { name: "Alexandre", phone: "(94) 98425-4300" },
+    { name: "Alexandre", phone: "(55) 93457-4300" },
     { name: "João", phone: "(93) 99158-6984" },
     { name: "Marcela", phone: "(62) 97845-8962" },
-    { name: "Iury", phone: "(94) 98425-4300" },
-    { name: "Guilherme", phone: "(94) 98425-4300" },
-    { name: "Lidia", phone: "(94) 98425-4300" },
-    { name: "Milena", phone: "(94) 98425-4300" },
+    { name: "Iury", phone: "(94) 98425-9837" },
+    { name: "Guilherme", phone: "(94) 92345-1230" },
+    { name: "Lidia", phone: "(94) 94879-4560" },
+    { name: "Milena", phone: "(94) 78457-3670" },
 ];
 
 function searchContact() {
   const mensageResult = document.querySelector(".mensage-result");
 
   for (i = 0; i < contacts.length; i++) {
-    if (contactName === "") {
+    if (contactName.value === "") {
       mensageResult.innerHTML = "Digite um nome de contato";
     } else if (contactName.value.toLowerCase() === contacts[i].name.toLowerCase()) {
       mensageResult.innerHTML = `Nome: ${contacts[i].name} <br> Contato: ${contacts[i].phone}`;
